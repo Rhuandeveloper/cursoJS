aafb8a3e7f98b00b15cf38de4525b8f6b8b7ca09
Rhuandeveloper <rhuanrodolfo13@gmail.com>
2024-09-29 18:08:56 -0300

    vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num =[5,8,4,7,6,8,10,9]
+num.push(1)
+num.sort()
+console.log(`o vetor tem ${num.length} pocisões`)
+console.log(`o primeiro valor do vetor é ${num[0]}`)
+console.log(`Nosso vetor é o ${num}`)
