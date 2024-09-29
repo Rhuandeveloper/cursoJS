3e3beba34ff6ea4b3e3ece0445a73703300aa9cc
Rhuandeveloper <rhuanrodolfo13@gmail.com>
2024-09-29 18:20:24 -0300

    maisvetores

--- aula15/vetor.js
+++ aula15/vetor.js
@@ -1,7 +1,11 @@
-let valores = [8,1,7,4,2,9]
+let num = [8,1,7,4,2,9]
 
 //console.log(valores)
 
-for(let pos = 0; pos < valores.length; pos++){
+/*for(let pos = 0; pos < valores.length; pos++){
     console.log(`a posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for(let pos in num){
+    console.log(num[pos])
 }
